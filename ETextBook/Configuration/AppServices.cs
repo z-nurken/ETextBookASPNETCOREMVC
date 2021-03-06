9c85de8a9de355f4008d4d9bab631d8ad317d26d
z-nurken <zulpykharov.nurken@gmail.com>
2021-03-06 18:39:21 +0600

    Initialize

---
+++ ETextBook/Configuration/AppServices.cs
@@ -0,0 +1,23 @@
+﻿using ETextBook.Data;
+using ETextBook.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETextBook.Configuration
+{
+    public static class AppServices
+    {
+        public static void AddDefaultServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddRazorPages();
+        }
+    }
+}
---
+++ ETextBook/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ETextBook.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent {
+        public async Task<IViewComponentResult> InvokeAsync() {
+            return await Task.Factory.StartNew(() => { return View(); });
+        }
+    }
+}
